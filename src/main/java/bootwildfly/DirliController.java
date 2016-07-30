4449854a93d9a509f74b37b5e1983cd66552602c
Jéssika Renally Ribeiro Rodrigues <reh.nally@gmail.com>
2016-07-30 00:53:19 -0300

    deleting some annotations

--- src/main/java/bootwildfly/DirliController.java
+++ src/main/java/bootwildfly/DirliController.java
@@ -3,13 +3,14 @@ package bootwildfly;
 import io.swagger.annotations.Api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api
 @RestController
 public class DirliController {
 	
-	@RequestMapping("hello")
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
     public String sayHello(){
         return ("Hello, Welcome to Dirli - A Submission System");
     }
